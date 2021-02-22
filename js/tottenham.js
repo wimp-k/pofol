9f9d3a5797e7ac26704724371c0849a1e9127f89
wimp-k <78093981+wimp-k@users.noreply.github.com>
2021-02-23 00:22:47 +0900

    Add files via upload

---
+++ js/tottenham.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('.box1 a').mouseover(function(){
+        $('section .minjae1').addClass('on')
+    })
+    $('.box1 a').mouseleave(function(){
+        $('section .minjae1').removeClass('on')
+    })
+
+    $('.box2 a').mouseover(function(){
+        $('section .minjae2').addClass('on')
+    })
+    $('.box2 a').mouseleave(function(){
+        $('section .minjae2').removeClass('on')
+    })
+
+    $('.box3 a').mouseover(function(){
+        $('section .minjae3').addClass('on')
+    })
+    $('.box3 a').mouseleave(function(){
+        $('section .minjae3').removeClass('on')
+    })
+
+    $('.box4 a').mouseover(function(){
+        $('section .minjae4').addClass('on')
+    })
+    $('.box4 a').mouseleave(function(){
+        $('section .minjae4').removeClass('on')
+    })
+    $('.box5 a').mouseover(function(){
+        $('section .minjae5').addClass('on')
+    })
+    $('.box5 a').mouseleave(function(){
+        $('section .minjae5').removeClass('on')
+    })
+    $('.box6 a').mouseover(function(){
+        $('section .minjae6').addClass('on')
+    })
+    $('.box6 a').mouseleave(function(){
+        $('section .minjae6').removeClass('on')
+    })
+    
+});
